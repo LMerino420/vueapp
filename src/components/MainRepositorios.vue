1a6f71c48da7b7d323a0c84542e3b2f687d0eec9
LMerino420 <merinolev96@gmail.com>
2022-07-15 17:06:13 -0600

    Propiedades computadas

--- src/components/MainRepositorios.vue
+++ src/components/MainRepositorios.vue
@@ -1,24 +1,44 @@
 <template>
 	<div>
-		Composition <br />
-		{{ texto }} <br />
-		{{ contador }}
+		<h3>Composition [Variables Reactivas]</h3>
+		[REF] ----> {{ texto }}<br />
+		[REACTIVE | WATCHER] ----> {{ contador }} <br />
+		[COMPUTED] ----> {{ fullName }}
 	</div>
 </template>
 
 <script>
-import { reactive, ref } from "vue";
+import { reactive, ref, watch, computed } from "vue";
 
 export default {
 	setup() {
 		const contador = reactive({ counter: 0 });
-		const texto = ref("Esto es un texto con ref");
+		const texto = ref("Texto con ref");
+		const firstName = ref("Lev");
+		const lastName = ref("Merino");
 
-		setInterval(() => contador.counter++, 500);
+		let inter = setInterval(() => contador.counter++, 400);
+
+		watch(
+			() => contador.counter,
+			(actual, anterior) => {
+				console.log("VAL INTER=>", actual);
+				console.log("Val ANT=>", anterior);
+				if (actual == 15) {
+					console.log("llego a su limite");
+					clearInterval(inter);
+				}
+			},
+		);
+
+		const fullName = computed(() => {
+			return `${firstName.value} ${lastName.value}`;
+		});
 
 		return {
 			contador,
 			texto,
+			fullName,
 		};
 	},
 };
