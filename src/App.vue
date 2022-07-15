4dd2a3100f2f511d5ebfaca5a481fc8e44fba2bd
LMerino420 <merinolev96@gmail.com>
2022-07-15 12:37:39 -0600

    AppVue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+	<div class="marg-div">
+		{{ textoCL }}
+	</div>
+	<div class="marg-div">
+		<button @click="show = !show">MENU</button>
+	</div>
+	<Transition name="fade">
+		<MainMenu v-show="show" />
+	</Transition>
+	<div class="marg-div">
+		<MainModal />
+	</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import MainMenu from "./components/MainMenu.vue";
+import MainModal from "./components/MainModal.vue";
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+	name: "App",
+	components: { MainMenu, MainModal },
+	data() {
+		return {
+			show: false,
+			textoCL: "Hola Vue app...",
+		};
+	},
+	beforeCreate() {
+		console.log("Before Create =>", this.data, this.$el);
+	},
+	created() {
+		console.log("Created =>", this.data, this.$el);
+	},
+	mounted() {
+		console.log("Mounted =>", this.data, this.$el);
+	},
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+
+.marg-div {
+	margin: 15px;
+}
+
+.fade-leave-to,
+.fade-enter-from {
+	opacity: 0;
+}
+
+.fade-leave-active,
+.fade-enter-active {
+	transition: opacity 0.5s ease;
 }
 </style>
---
+++ src/components/MainModal.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<button @click="toggle">Modal</button>
+		<teleport to="body">
+			<div v-show="showM" class="modal">
+				<h1>Titulo</h1>
+				<p>Lorem ipsum dolor sit amet consectetur.</p>
+				<button @click="toggle">Cerrar</button>
+			</div>
+		</teleport>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			showM: false,
+		};
+	},
+	methods: {
+		toggle() {
+			this.showM = !this.showM;
+		},
+	},
+};
+</script>
